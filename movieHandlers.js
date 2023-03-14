9749f0df0569904c28e5ce80de8353736b41a468
MaxGuet <maxence.guet@gmail.com>
2023-03-14 10:31:57 +0100

    commit

--- movieHandlers.js
+++ movieHandlers.js
@@ -1,32 +1,42 @@
+const database = require('./database');
+
 const movies = [
   {
     id: 1,
-    title: "Citizen Kane",
-    director: "Orson Wells",
-    year: "1941",
+    title: 'Citizen Kane',
+    director: 'Orson Wells',
+    year: '1941',
     colors: false,
     duration: 120,
   },
   {
     id: 2,
-    title: "The Godfather",
-    director: "Francis Ford Coppola",
-    year: "1972",
+    title: 'The Godfather',
+    director: 'Francis Ford Coppola',
+    year: '1972',
     colors: true,
     duration: 180,
   },
   {
     id: 3,
-    title: "Pulp Fiction",
-    director: "Quentin Tarantino",
-    year: "1994",
+    title: 'Pulp Fiction',
+    director: 'Quentin Tarantino',
+    year: '1994',
     color: true,
     duration: 180,
   },
 ];
 
 const getMovies = (req, res) => {
-  res.json(movies);
+  database
+    .query('select * from movies')
+    .then(([movies]) => {
+      res.json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error retrieving data from database');
+    });
 };
 
 const getMovieById = (req, res) => {
@@ -37,7 +47,7 @@ const getMovieById = (req, res) => {
   if (movie != null) {
     res.json(movie);
   } else {
-    res.status(404).send("Not Found");
+    res.status(404).send('Not Found');
   }
 };
 
