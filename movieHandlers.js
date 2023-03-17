6ef24df6c7186ef0efa779dbac9623fa727763df
MaxGuet <maxence.guet@gmail.com>
2023-03-17 10:58:28 +0100

    Express05

--- movieHandlers.js
+++ movieHandlers.js
@@ -150,6 +150,41 @@ const updateUser = (req, res) => {
     });
 };
 
+const deleteMovie = (req, res) => {
+  const id = parseInt(req.params.id);
+  database
+    .query('delete from movies where id = ?', [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send('Not found');
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error deleting the movie');
+    });
+};
+
+const deleteUser = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query('delete from users where id=?', [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send('Not found');
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send('Error deleting the element');
+    });
+};
+
 module.exports = {
   getMovies,
   getMovieById,
@@ -159,4 +194,6 @@ module.exports = {
   postUser,
   updateMovie,
   updateUser,
+  deleteMovie,
+  deleteUser,
 };
